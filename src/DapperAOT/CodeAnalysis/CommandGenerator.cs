667beca2a9dd6337cbab17fbb7efdc68f51bb26b
mgravell <marc.gravell@gmail.com>
2021-04-26 15:26:32 +0100

    handles types / namespaces in output

--- src/DapperAOT/CodeAnalysis/CommandGenerator.cs
+++ src/DapperAOT/CodeAnalysis/CommandGenerator.cs
@@ -3,6 +3,8 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 
@@ -57,7 +59,7 @@ namespace DapperAOT.CodeAnalysis
         void ISourceGenerator.Execute(GeneratorExecutionContext context)
         {
             if (context.SyntaxReceiver is not CommandSyntaxReceiver rec || rec.IsEmpty) return;
-            var sb = new StringBuilder();
+            List<(string Namespace, string TypeName, IMethodSymbol Method)>? candidates = null;
             foreach (var syntaxNode in rec)
             {
                 try
@@ -67,11 +69,13 @@ namespace DapperAOT.CodeAnalysis
 
                     if (method.PartialImplementationPart is not null) continue; // already has an implementation
 
+                    if (method.IsGenericMethod) continue; // generics not implemented yet
+
                     var ca = method.GetAttributes().SingleOrDefault(x => IsCommandAttribute(x.AttributeClass));
                     if (ca is null) continue; // lacking [Command]
 
                     Log?.Invoke($"Detected candidate: '{method.Name}'");
-                    sb.Append("// ").Append(method.Name).AppendLine();
+                    (candidates ??= new()).Add((GetNamespace(method.ContainingType), GetTypeName(method.ContainingType), method));
                 }
                 catch (Exception ex)
                 {
@@ -81,10 +85,161 @@ namespace DapperAOT.CodeAnalysis
                     context.ReportDiagnostic(err);
                 }
             }
-            context.AddSource("DapperAOT.generated.cs", sb.ToString());
+            if (candidates is not null)
+            {
+                candidates.Sort((x, y) =>
+                {
+                    int delta = string.Compare(x.Namespace, y.Namespace, StringComparison.Ordinal);
+                    if (delta == 0) delta = string.Compare(x.TypeName, y.TypeName, StringComparison.Ordinal);
+                    return delta;
+                });
+                var generated = Generate(candidates);
+                context.AddSource("DapperAOT.generated.cs", generated);
+            }   
 
             static bool IsCommandAttribute(INamedTypeSymbol type)
                 => type?.Name == "CommandAttribute" && type.ContainingNamespace?.Name == "Dapper";
+
+            static string GetNamespace(INamedTypeSymbol? type)
+            {
+                static bool IsRoot([NotNullWhen(false)] INamespaceSymbol? ns)
+                    => ns is null || ns.IsGlobalNamespace;
+
+                var ns = type?.ContainingNamespace;
+                if (IsRoot(ns)) return "";
+                if (IsRoot(ns.ContainingNamespace)) return ns.Name;
+                var sb = new StringBuilder();
+                WriteAncestry(ns.ContainingNamespace, sb);
+                sb.Append(ns.Name);
+                return sb.ToString();
+
+                static void WriteAncestry(INamespaceSymbol? ns, StringBuilder sb)
+                {
+                    if (!IsRoot(ns))
+                    {
+                        WriteAncestry(ns.ContainingNamespace, sb);
+                        sb.Append(ns.Name).Append('.');
+                    }
+                }
+            }
+            static string GetTypeName(INamedTypeSymbol? type)
+            {
+                var parent = type?.ContainingType;
+                if (parent is null) return type?.Name ?? "";
+                var sb = new StringBuilder();
+                WriteAncestry(parent, sb);
+                sb.Append(type?.Name);
+                return sb.ToString();
+
+                static void WriteAncestry(INamedTypeSymbol? parent, StringBuilder sb)
+                {
+                    if (parent is not null)
+                    {
+                        WriteAncestry(parent.ContainingType, sb);
+                        sb.Append(parent.Name).Append('.');
+                    }
+                }
+            }
+        }
+
+        [SuppressMessage("Style", "IDE0042:Deconstruct variable declaration", Justification = "Clarity")]
+        [SuppressMessage("Style", "IDE0057:Use range operator", Justification = "Clarity")]
+        private string Generate(List<(string Namespace, string TypeName, IMethodSymbol Method)> candidates)
+        {
+
+            var sb = new StringBuilder();
+            int indent = 0;
+            StringBuilder NewLine()
+                => sb.AppendLine().Append('\t', indent);
+
+            foreach (var nsGrp in candidates.GroupBy(x => x.Namespace))
+            {
+                Log?.Invoke($"Namespace '{nsGrp.Key}' has {nsGrp.Count()} candidate(s) in {nsGrp.Select(x => x.TypeName).Distinct().Count()} type(s)");
+
+                if (!string.IsNullOrWhiteSpace(nsGrp.Key))
+                {
+                    NewLine().Append("namespace ").Append(nsGrp.Key);
+                    NewLine().Append('{');
+                    indent++;
+                }
+
+                foreach (var typeGrp in nsGrp.GroupBy(x => x.TypeName))
+                {
+                    int typeCount = 0;
+                    ReadOnlySpan<char> span = typeGrp.Key;
+                    int ix;
+                    while ((ix = span.IndexOf('.')) > 0)
+                    {
+                        NewLine().Append("partial class ").Append(span.Slice(0, ix));
+                        NewLine().Append('{');
+                        indent++;
+                        typeCount++;
+                        span = span.Slice(ix + 1);
+                    }
+                    NewLine().Append("partial class ").Append(span);
+                    NewLine().Append('{');
+                    indent++;
+                    typeCount++;
+
+                    bool firstMethod = true;
+                    foreach (var candidate in typeGrp)
+                    {
+                        if (!firstMethod)
+                        {
+                            NewLine();
+                        }
+                        firstMethod = false;
+                        WriteMethod(candidate.Method, sb, indent);
+                    }
+
+                    while (typeCount > 0)
+                    {
+                        indent--;
+                        typeCount--;
+                        NewLine().Append('}');
+                    }
+                }
+
+                if (!string.IsNullOrWhiteSpace(nsGrp.Key))
+                {
+                    indent--;
+                    NewLine().Append('}');
+                }
+            }
+            return sb.ToString();
+        }
+
+        private void WriteMethod(IMethodSymbol method, StringBuilder sb, int indent)
+        {
+            StringBuilder NewLine()
+                => sb.AppendLine().Append('\t', indent);
+
+            NewLine().Append(method.DeclaredAccessibility switch
+            {
+                Accessibility.Public => "public",
+                Accessibility.Internal => "internal",
+                Accessibility.Private => "private",
+                Accessibility.ProtectedAndInternal => "private protected",
+                Accessibility.Protected => "public",
+                Accessibility.ProtectedOrInternal => "protected internal",
+                _ => method.DeclaredAccessibility.ToString(), // expect this to cause an error, that's OK
+            }).Append(' ').Append(method.ReturnType.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat)).Append(' ').Append(method.Name).Append("(");
+            bool first = true;
+            foreach (var p in method.Parameters)
+            {
+                if (!first) sb.Append(", ");
+                first = false;
+                sb.Append(p.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat));
+            }
+            sb.Append(")");
+            NewLine().Append('{');
+            indent++;
+
+            NewLine().Append("// TODO");
+
+            indent--;
+            NewLine().Append('}');
+                
         }
 
         void ISourceGenerator.Initialize(GeneratorInitializationContext context)
--- test/DapperAOT.Test/DetectMethodsTests.cs
+++ test/DapperAOT.Test/DetectMethodsTests.cs
@@ -16,16 +16,40 @@ namespace DapperAOT.Test
         {
             var result = Execute<CommandGenerator>(@"
 using Dapper;
+using System.Data;
 class Foo {
-    partial void ShouldIgnoreThis_NoAccessibility();
-    [Command]
-    public partial int ShouldDetectThis();
+    partial void ShouldIgnoreThis_NoAccessibility(DbConnection connection, string region);
+    [Command(""select * from Customers where Region = @region"")]
+    public partial int ShouldDetectThis(DbConnection connection, string region);
 
-    public partial int ShouldIgnoreThis_NoAttribute();
+    public partial int ShouldIgnoreThis_NoAttribute(DbConnection connection, string region);
 
     [Command]
-    public partial int ShouldIgnoreThis_HasImplementation();
-    public partial int ShouldIgnoreThis_HasImplementation() => 42;
+    public partial int ShouldIgnoreThis_HasImplementation(DbConnection connection, string region);
+    public partial int ShouldIgnoreThis_HasImplementation(DbConnection connection, string region) => 42;
+}
+
+namespace X.Y.Z
+{
+    partial class A
+    {
+        partial class B
+        {
+            [Command(""select * from Customers where Region = @region"")]
+            public partial int ShouldDetectThisInB(DbConnection connection, string region);
+        }
+    }
+}
+namespace X.Y.Z
+{
+    partial class A
+    {
+        partial class B
+        {
+           [Command(""select * from Customers where Region = @region"")]
+            public partial int ShouldAlsoDetectThisInB(DbConnection connection, string region);
+        }
+    }
 }
 ");
             Assert.Empty(result.Diagnostics);
