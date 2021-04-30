edc43a688f625bb02fbbead6f473620ec4d65efe
mgravell <marc.gravell@gmail.com>
2021-04-30 13:49:42 +0100

    wipe scratch

--- test/Dapper.AOT.Test/Samples/Scratch.cs
+++ test/Dapper.AOT.Test/Samples/Scratch.cs
@@ -1,17 +1,1 @@
-﻿using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using System.Threading;
-using System.Threading.Tasks;
-
-partial class P
-
-{
-	public partial IAsyncEnumerable<int> Foo(CancellationToken ct);
-
-	public async partial IAsyncEnumerable<int> Foo([EnumeratorCancellation] CancellationToken ct)
-	{
-		yield return 1;
-		await Task.Yield();
-		yield return 2;
-	}
-}
+﻿
