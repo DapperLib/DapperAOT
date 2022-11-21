5daa584a30b852f88840fedfca30add231c32193
Marc Gravell <marc.gravell@gmail.com>
2022-11-21 16:20:43 +0000

    fixup benchmarks etc
    |                                 Method |                        Categories |      Mean |    Error |   StdDev |   Gen0 | Allocated |
    |--------------------------------------- |---------------------------------- |----------:|---------:|---------:|-------:|----------:|
    |      DapperAOTMicrosoftData_PooledTask |       PooledTask,MSData,DapperAOT | 130.03 us | 2.495 us | 2.334 us | 0.2500 |   3.25 KB |
    |         DapperAOTSystemData_PooledTask |      PooledTask,SysData,DapperAOT | 111.56 us | 1.010 us | 0.895 us | 0.4000 |   4.02 KB |
    | DapperAOTMicrosoftData_PooledValueTask |  PooledValueTask,MSData,DapperAOT | 126.75 us | 1.288 us | 1.142 us | 0.2500 |   3.18 KB |
    |    DapperAOTSystemData_PooledValueTask | PooledValueTask,SysData,DapperAOT | 110.99 us | 0.945 us | 0.838 us | 0.4000 |   3.95 KB |
    |                    DapperMicrosoftData |                Sync,MSData,Dapper | 103.19 us | 1.047 us | 0.875 us | 0.4000 |   3.27 KB |
    |                 DapperAOTMicrosoftData |             Sync,MSData,DapperAOT |  99.89 us | 0.960 us | 0.851 us | 0.2000 |   1.84 KB |
    |                       DapperSystemData |               Sync,SysData,Dapper |  87.21 us | 0.613 us | 0.573 us | 0.3333 |   3.18 KB |
    |                    DapperAOTSystemData |            Sync,SysData,DapperAOT |  84.58 us | 1.223 us | 1.144 us | 0.1667 |   1.88 KB |
    |              DapperMicrosoftData_Async |                Task,MSData,Dapper | 128.48 us | 1.438 us | 1.345 us | 0.5000 |   5.04 KB |
    |            DapperAOTMicrosoftData_Task |             Task,MSData,DapperAOT | 127.73 us | 1.561 us | 1.384 us | 0.2500 |   3.42 KB |
    |                 DapperSystemData_Async |               Task,SysData,Dapper | 112.69 us | 1.202 us | 1.004 us | 0.6000 |   6.13 KB |
    |               DapperAOTSystemData_Task |            Task,SysData,DapperAOT | 111.97 us | 1.173 us | 1.040 us | 0.4000 |   4.19 KB |
    |       DapperAOTMicrosoftData_ValueTask |        ValueTask,MSData,DapperAOT | 128.08 us | 2.401 us | 2.246 us | 0.2500 |   3.18 KB |
    |          DapperAOTSystemData_ValueTask |       ValueTask,SysData,DapperAOT | 111.42 us | 0.802 us | 0.751 us | 0.4000 |   3.95 KB |

--- test/UsageBenchmark/Program.cs
+++ test/UsageBenchmark/Program.cs
@@ -4,10 +4,7 @@ using BenchmarkDotNet.Running;
 using Dapper;
 using PooledAwait;
 using System;
-using System.Data;
 using System.Data.Common;
-using System.Globalization;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 // [module: LegacyMaterializer(true)]
@@ -29,7 +26,7 @@ namespace UsageBenchmark
         [GlobalSetup]
         public void Connect()
         {
-            const string cs = @"Data Source=.\SQLEXPRESS;Initial Catalog=master;Integrated Security=True";
+            const string cs = @"Data Source=.\SQLEXPRESS;Initial Catalog=master;Integrated Security=True;TrustServerCertificate=True";
             _msData = new Microsoft.Data.SqlClient.SqlConnection(cs);
             _msData.Open();
             _systemData = new System.Data.SqlClient.SqlConnection(cs);
@@ -200,92 +197,92 @@ namespace UsageBenchmark
     public sealed class Customer
     {
         // [Column("CustomerId")]
-        public int Id { get; init; }
-        public string? Name { get; init; }
-        public string? Region { get; init; }
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Region { get; set; }
 
-        //TODO generate this automatically
-        internal sealed class CustomerReader : TypeReader<Customer>
-        {
-            [ModuleInitializer]
-            internal static void Register() => Register(Instance);
-            private CustomerReader() { }
-            internal static CustomerReader Instance { get; } = new CustomerReader();
-            protected override int GetColumnToken(string name, Type? type, bool isNullable)
-            {
-                switch (name)
-                {
-                    case nameof(Id):
-                        if (type == typeof(int)) return isNullable ? 0 : 1;
-                        return 2;
-                    case nameof(Name):
-                        if (type == typeof(int)) return isNullable ? 3 : 4;
-                        return 5;
-                    case nameof(Region):
-                        if (type == typeof(int)) return isNullable ? 6 : 7;
-                        return 8;
-                    default:
-                        return NoField;
-                }
-            }
+        ////TODO generate this automatically
+        //internal sealed class CustomerReader : TypeReader<Customer>
+        //{
+        //    [ModuleInitializer]
+        //    internal static void Register() => Register(Instance);
+        //    private CustomerReader() { }
+        //    internal static CustomerReader Instance { get; } = new CustomerReader();
+        //    protected override int GetColumnToken(string name, Type? type, bool isNullable)
+        //    {
+        //        switch (name)
+        //        {
+        //            case nameof(Id):
+        //                if (type == typeof(int)) return isNullable ? 0 : 1;
+        //                return 2;
+        //            case nameof(Name):
+        //                if (type == typeof(int)) return isNullable ? 3 : 4;
+        //                return 5;
+        //            case nameof(Region):
+        //                if (type == typeof(int)) return isNullable ? 6 : 7;
+        //                return 8;
+        //            default:
+        //                return NoField;
+        //        }
+        //    }
 
-            public override Customer Read(DbDataReader reader, ReadOnlySpan<int> tokens, int offset)
-            {
-                int Id = default;
-                string? Name = default;
-                string? Region = default;
-                foreach (var token in tokens)
-                {
-                    switch (token)
-                    {
-                        case 0: Id = reader.GetInt32(offset); break;
-                        case 1: Id = reader.GetInt32(offset); break;
-                        case 2: Id = Convert.ToInt32(reader.GetValue(offset), CultureInfo.InvariantCulture); break;
-                        case 3: Name = reader.GetString(offset); break;
-                        case 4: Name = reader.IsDBNull(offset) ? null : reader.GetString(offset); break;
-                        case 5: Name = reader.IsDBNull(offset) ? null : Convert.ToString(reader.GetValue(offset), CultureInfo.InvariantCulture); break;
-                        case 6: Region = reader.GetString(offset); break;
-                        case 7: Region = reader.IsDBNull(offset) ? null : reader.GetString(offset); break;
-                        case 8: Region = reader.IsDBNull(offset) ? null : Convert.ToString(reader.GetValue(offset), CultureInfo.InvariantCulture); break;
-                    }
-                    offset++;
-                }
-                return new Customer
-                {
-                    Id = Id,
-                    Name = Name,
-                    Region = Region
-                };
-            }
+        //    public override Customer Read(DbDataReader reader, ReadOnlySpan<int> tokens, int offset)
+        //    {
+        //        int Id = default;
+        //        string? Name = default;
+        //        string? Region = default;
+        //        foreach (var token in tokens)
+        //        {
+        //            switch (token)
+        //            {
+        //                case 0: Id = reader.GetInt32(offset); break;
+        //                case 1: Id = reader.GetInt32(offset); break;
+        //                case 2: Id = Convert.ToInt32(reader.GetValue(offset), CultureInfo.InvariantCulture); break;
+        //                case 3: Name = reader.GetString(offset); break;
+        //                case 4: Name = reader.IsDBNull(offset) ? null : reader.GetString(offset); break;
+        //                case 5: Name = reader.IsDBNull(offset) ? null : Convert.ToString(reader.GetValue(offset), CultureInfo.InvariantCulture); break;
+        //                case 6: Region = reader.GetString(offset); break;
+        //                case 7: Region = reader.IsDBNull(offset) ? null : reader.GetString(offset); break;
+        //                case 8: Region = reader.IsDBNull(offset) ? null : Convert.ToString(reader.GetValue(offset), CultureInfo.InvariantCulture); break;
+        //            }
+        //            offset++;
+        //        }
+        //        return new Customer
+        //        {
+        //            Id = Id,
+        //            Name = Name,
+        //            Region = Region
+        //        };
+        //    }
 
-            protected override Customer ReadFallback(IDataReader reader, ReadOnlySpan<int> tokens, int offset)
-            {
-                int Id = default;
-                string? Name = default;
-                string? Region = default;
-                foreach (var token in tokens)
-                {
-                    switch (token)
-                    {
-                        case 0: Id = reader.GetInt32(offset); break;
-                        case 1: Id = reader.GetInt32(offset); break;
-                        case 2: Id = Convert.ToInt32(reader.GetValue(offset), CultureInfo.InvariantCulture); break;
-                        case 3: Name = reader.GetString(offset); break;
-                        case 4: Name = reader.IsDBNull(offset) ? null : reader.GetString(offset); break;
-                        case 5: Name = reader.IsDBNull(offset) ? null : Convert.ToString(reader.GetValue(offset), CultureInfo.InvariantCulture); break;
-                        case 6: Region = reader.GetString(offset); break;
-                        case 7: Region = reader.IsDBNull(offset) ? null : reader.GetString(offset); break;
-                        case 8: Region = reader.IsDBNull(offset) ? null : Convert.ToString(reader.GetValue(offset), CultureInfo.InvariantCulture); break;
-                    }
-                    offset++;
-                }
-                return new Customer
-                {
-                    Id = Id,
-                    Name = Name,
-                    Region = Region
-                };
-            }
-        }
+        //    protected override Customer ReadFallback(IDataReader reader, ReadOnlySpan<int> tokens, int offset)
+        //    {
+        //        int Id = default;
+        //        string? Name = default;
+        //        string? Region = default;
+        //        foreach (var token in tokens)
+        //        {
+        //            switch (token)
+        //            {
+        //                case 0: Id = reader.GetInt32(offset); break;
+        //                case 1: Id = reader.GetInt32(offset); break;
+        //                case 2: Id = Convert.ToInt32(reader.GetValue(offset), CultureInfo.InvariantCulture); break;
+        //                case 3: Name = reader.GetString(offset); break;
+        //                case 4: Name = reader.IsDBNull(offset) ? null : reader.GetString(offset); break;
+        //                case 5: Name = reader.IsDBNull(offset) ? null : Convert.ToString(reader.GetValue(offset), CultureInfo.InvariantCulture); break;
+        //                case 6: Region = reader.GetString(offset); break;
+        //                case 7: Region = reader.IsDBNull(offset) ? null : reader.GetString(offset); break;
+        //                case 8: Region = reader.IsDBNull(offset) ? null : Convert.ToString(reader.GetValue(offset), CultureInfo.InvariantCulture); break;
+        //            }
+        //            offset++;
+        //        }
+        //        return new Customer
+        //        {
+        //            Id = Id,
+        //            Name = Name,
+        //            Region = Region
+        //        };
+        //    }
+        //}
     }
 }
--- test/UsageLinker/Program.cs
+++ test/UsageLinker/Program.cs
@@ -1,19 +1,16 @@
 ﻿using Dapper;
 using Microsoft.Data.SqlClient;
 using System;
-using System.Data;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Common;
-using System.Globalization;
 
 [module: LegacyMaterializer(false)]
 namespace UsageLinker
 {
-    static class Program
+	static class Program
 	{
 		static void Main()
 		{
-            //TypeReader.Register(Customer.CustomerReader.Instance);
-
             using var conn = new SqlConnection("gibberish");
 			var c = Customer.GetCustomer(conn, 42, "South");
 			Console.WriteLine(c.Name);
@@ -26,90 +23,9 @@ namespace UsageLinker
 		public static partial Customer GetCustomer(DbConnection connection, int id, string region);
 
 
-		// [Column("CustomerId")]
-		public int Id { get; init; }
-		public string? Name { get; init; }
-		public string? Region { get; init; }
-
-        public sealed class CustomerReader : TypeReader<Customer>
-        {
-            private CustomerReader() { }
-            public static CustomerReader Instance { get; } = new CustomerReader();
-            protected override int GetColumnToken(string name, Type? type, bool isNullable)
-            {
-                switch (name)
-                {
-                    case nameof(Id):
-                        if (type == typeof(int)) return isNullable ? 0 : 1;
-                        return 2;
-                    case nameof(Name):
-                        if (type == typeof(int)) return isNullable ? 3 : 4;
-                        return 5;
-                    case nameof(Region):
-                        if (type == typeof(int)) return isNullable ? 6 : 7;
-                        return 8;
-                    default:
-                        return NoField;
-                }
-            }
-
-            public override Customer Read(DbDataReader reader, ReadOnlySpan<int> tokens, int offset)
-            {
-                int Id = default;
-                string? Name = default;
-                string? Region = default;
-                foreach (var token in tokens)
-                {
-                    switch (token)
-                    {
-                        case 0: Id = reader.GetInt32(offset); break;
-                        case 1: Id = reader.GetInt32(offset); break;
-                        case 2: Id = Convert.ToInt32(reader.GetValue(offset), CultureInfo.InvariantCulture); break;
-                        case 3: Name = reader.GetString(offset); break;
-                        case 4: Name = reader.IsDBNull(offset) ? null : reader.GetString(offset); break;
-                        case 5: Name = reader.IsDBNull(offset) ? null : Convert.ToString(reader.GetValue(offset), CultureInfo.InvariantCulture); break;
-                        case 6: Region = reader.GetString(offset); break;
-                        case 7: Region = reader.IsDBNull(offset) ? null : reader.GetString(offset); break;
-                        case 8: Region = reader.IsDBNull(offset) ? null : Convert.ToString(reader.GetValue(offset), CultureInfo.InvariantCulture); break;
-                    }
-                    offset++;
-                }
-                return new Customer
-                {
-                    Id = Id,
-                    Name = Name,
-                    Region = Region
-                };
-            }
-
-            protected override Customer ReadFallback(IDataReader reader, ReadOnlySpan<int> tokens, int offset)
-            {
-                int Id = default;
-                string? Name = default;
-                string? Region = default;
-                foreach (var token in tokens)
-                {
-                    switch (token)
-                    {
-                        case 0: Id = reader.GetInt32(offset); break;
-                        case 1: Id = reader.GetInt32(offset); break;
-                        case 2: Id = Convert.ToInt32(reader.GetValue(offset), CultureInfo.InvariantCulture); break;
-                        case 3: Name = reader.GetString(offset); break;
-                        case 4: Name = reader.IsDBNull(offset) ? null : reader.GetString(offset); break;
-                        case 5: Name = reader.IsDBNull(offset) ? null : Convert.ToString(reader.GetValue(offset), CultureInfo.InvariantCulture); break;
-                        case 6: Region = reader.GetString(offset); break;
-                        case 7: Region = reader.IsDBNull(offset) ? null : reader.GetString(offset); break;
-                        case 8: Region = reader.IsDBNull(offset) ? null : Convert.ToString(reader.GetValue(offset), CultureInfo.InvariantCulture); break;
-                    }
-                    offset++;
-                }
-                return new Customer
-                {
-                    Id = Id,
-                    Name = Name,
-                    Region = Region
-                };
-            }
-        }
+		[Column("CustomerId")]
+		public int Id { get; set; }
+		public string? Name { get; set; }
+		public string? Region { get; set; }
     }
 }
