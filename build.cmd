973c9f0882a235fac6830becddb7335ee04c3c53
Marc Gravell <marc.gravell@gmail.com>
2023-11-12 18:44:49 +0000

    fixup linker (libref)

--- build.cmd
+++ build.cmd
@@ -1,11 +1,15 @@
 @cls
+@rd /S /Q "src/Dapper.AOT.Analyzers/bin/Release/" >NUL 2>NUL
 @rd /S /Q "src/Dapper.AOT/bin/Release/" >NUL 2>NUL
+@rd /S /Q "src/Dapper.Advisor/bin/Release/" >NUL 2>NUL
 
 @dotnet clean src/Dapper.AOT.Analyzers
 @dotnet clean src/Dapper.AOT
+@dotnet clean src/Dapper.Advisor
 
 @dotnet restore src/Dapper.AOT.Analyzers
 @dotnet restore src/Dapper.AOT
+@dotnet restore src/Dapper.Advisor
 
 @dotnet build src/Dapper.AOT.Analyzers -c Release
 @dotnet build src/Dapper.AOT -c Release
@@ -18,5 +22,7 @@
 
 @echo .
 @echo Deployment package is in: src/Dapper.AOT/bin/Release/
+@echo and: src/Dapper.Advisor/bin/Release/
 @echo .
 @dir src\Dapper.AOT\bin\Release\*.nupkg /B
+@dir src\Dapper.Advisor\bin\Release\*.nupkg /B
--- test/Dapper.AOT.Test/Interceptors/QueryCustomConstructionWithFactoryMethod.output.cs
+++ test/Dapper.AOT.Test/Interceptors/QueryCustomConstructionWithFactoryMethod.output.cs
@@ -242,38 +242,38 @@ namespace Dapper.AOT // interceptors must be in a known namespace
             }
             public override global::Foo.SingleFactoryNotMarkedWithDapperAot Read(global::System.Data.Common.DbDataReader reader, global::System.ReadOnlySpan<int> tokens, int columnOffset, object? state)
             {
-                int value0 = default;
-                string? value1 = default;
-                double? value2 = default;
+                global::Foo.SingleFactoryNotMarkedWithDapperAot result = new();
                 foreach (var token in tokens)
                 {
                     switch (token)
                     {
                         case 0:
-                            value0 = reader.GetInt32(columnOffset);
+                            result.X = reader.GetInt32(columnOffset);
                             break;
                         case 3:
-                            value0 = GetValue<int>(reader, columnOffset);
+                            result.X = GetValue<int>(reader, columnOffset);
                             break;
                         case 1:
-                            value1 = reader.IsDBNull(columnOffset) ? (string?)null : reader.GetString(columnOffset);
+                            result.Y = reader.IsDBNull(columnOffset) ? (string?)null : reader.GetString(columnOffset);
                             break;
                         case 4:
-                            value1 = reader.IsDBNull(columnOffset) ? (string?)null : GetValue<string>(reader, columnOffset);
+                            result.Y = reader.IsDBNull(columnOffset) ? (string?)null : GetValue<string>(reader, columnOffset);
                             break;
                         case 2:
-                            value2 = reader.IsDBNull(columnOffset) ? (double?)null : reader.GetDouble(columnOffset);
+                            result.Z = reader.IsDBNull(columnOffset) ? (double?)null : reader.GetDouble(columnOffset);
                             break;
                         case 5:
-                            value2 = reader.IsDBNull(columnOffset) ? (double?)null : GetValue<double>(reader, columnOffset);
+                            result.Z = reader.IsDBNull(columnOffset) ? (double?)null : GetValue<double>(reader, columnOffset);
                             break;
 
                     }
                     columnOffset++;
 
                 }
-                return global::Foo.SingleFactoryNotMarkedWithDapperAot.Construct(value0, value1, value2);
+                return result;
+
             }
+
         }
 
         private sealed class RowFactory3 : global::Dapper.RowFactory<global::Foo.MultipleStandardFactoryMethods>
