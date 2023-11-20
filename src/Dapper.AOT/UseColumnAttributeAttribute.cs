f692ffe0b022d898b122da69a600e758a389b21f
Dmitrii Korolev <dmkorolev@microsoft.com>
2023-11-20 18:22:21 +0100

    add attr usage

--- src/Dapper.AOT/UseColumnAttributeAttribute.cs
+++ src/Dapper.AOT/UseColumnAttributeAttribute.cs
@@ -5,6 +5,7 @@ namespace Dapper
     /// <summary>
     /// Specifies whether to use [System.ComponentModel.DataAnnotations.Schema.ColumnAttribute] for additional behavioral configuration
     /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
     public class UseColumnAttributeAttribute : Attribute
     {
         /// <param name="enable">enable usage of [Column] attribute</param>
