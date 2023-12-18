20659c16613a3033a13f95982f368dca6ccc66c1
Marc Gravell <marc.gravell@gmail.com>
2023-12-18 14:02:10 +0000

    support inherited members (#106)
    fix #93

---
+++ test/Dapper.AOT.Test/Interceptors/InheritedMembers.input.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using System.Data.Common;
+
+[DapperAot]
+public static class Foo
+{
+    static void SomeCode(DbConnection connection, string bar)
+    {
+        var args = new { Foo = 12, bar };
+        // these should support Id+Name
+        var obj = connection.QueryFirst<Entity1>("def", args);
+        connection.Execute("ghi @Id, @Name", obj);
+    }
+}
+
+public abstract class EntityBase
+{
+    public long Id { get; set; }
+}
+
+public class Entity1 : EntityBase
+{
+    public string Name { get; set; }
+}
